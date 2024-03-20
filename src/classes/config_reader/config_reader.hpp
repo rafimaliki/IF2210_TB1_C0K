6138106f38c2d30ea0984cb87d3ab0597f159b08
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-03-20 10:59:14 +0700

    file reader broken

---
+++ src/classes/config_reader/config_reader.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <filesystem>
+
+class ConfigReader {
+public:
+    ConfigReader(const std::string& filename) : filename(filename) {}
+
+    std::vector<std::string> readLines() {
+
+
+        // Convert the path to a string and print it
+        // std::cout << "Current working directory: " << std::filesystem::current_path().string() << std::endl;
+
+        std::vector<std::string> lines;
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file: " << filename << std::endl;
+            return lines; // Return empty vector if file cannot be opened
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            lines.push_back(line);
+        }
+
+        file.close();
+        return lines;
+    }
+
+private:
+    std::string filename;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
+// File: main.cpp
+
 #include <iostream>
+#include <vector>
+#include <string>
+
+#include "classes/config_reader/config_reader.hpp"
 
 using std::cout;
 using std::endl;
+using std::vector;
+using std::string;
 
 int main() {
+
     cout << "Welcome to FARM CITY!" << endl;
-    
+
+    ConfigReader animal_config("../config/animals.txt");
+    vector<string> animal_data = animal_config.readLines();
+
+    for (const auto& line : animal_data) {
+        cout << line << endl;
+    }
+
+
+
     return 0;
 }
