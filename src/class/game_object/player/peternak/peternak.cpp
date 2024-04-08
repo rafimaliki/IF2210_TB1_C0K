029ff53a6a1d394fd6caac62ff90a57146e85824
TuanOnta <mdz54dzakiarta@gmail.com>
2024-04-08 17:55:12 +0700

    feat: CETAK_PETERNAKAN

--- src/class/game_object/player/peternak/peternak.cpp
+++ src/class/game_object/player/peternak/peternak.cpp
@@ -15,6 +15,26 @@ void Peternak::addAnimal(Item* item){
 
 void Peternak::CETAK_PETERNAKAN(){  /* BELUM DETAIL SESUAI SPEK */
     this->peternakan.print();
+    cout << endl;
+    int height = this->peternakan.height;
+    int width = this->peternakan.width;
+    for (int i = 0 ;i < height; i++){
+        for (int j = 0; j < width; j++){
+            if (!this->peternakan.isEmpty(i, j)){
+                cout << "- " << i + "A";
+                if (j < 10){
+                    cout << "0" << j <<": ";
+                    cout << this->peternakan.getItem(i, j)->getKODE_HURUF();
+                }else{
+                    cout << j <<": ";
+                    cout << this->peternakan.getItem(i, j)->getKODE_HURUF();
+                }
+                cout << endl <<endl;
+            }
+        }
+    }
+
+
 }
 void Peternak::TERNAK(){  /* BELUM IMPLEMENTASI */
     cout << YELLOW << "\nCommand TERNAK belum diimplementasikan!\n" << RESETstring << endl;
