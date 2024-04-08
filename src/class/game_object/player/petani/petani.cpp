09065fb97805dc3d8e332b6ce608abb0532e1fa3
Muhammad Zaki <muhammadzaki9@ymail.com>
2024-04-08 13:28:21 +0700

    feat: add CETAK_LADANG

--- src/class/game_object/player/petani/petani.cpp
+++ src/class/game_object/player/petani/petani.cpp
@@ -18,11 +18,77 @@ Inventory<Plant>* Petani::getLahan(){
 /* Game command related methods */
 
 void Petani::CETAK_LADANG(){ /* BELUM DETAIL SESUAI SPEK */
+
+    //Temp untuk menyimpan tanaman yang unik saja
+    map<string,string> temp_tanaman_unik;
+
+
     this->lahan.print();
+
+    //Cari ada tanaman apa saja di ladang
+    for(int i = 0 ; i < this->lahan.height; i++){
+        for(int j = 0 ; j < this->lahan.width; j++){
+            if(this->lahan.grid[i][j].getItem() != nullptr){
+                string nama_tanaman = this->lahan.grid[i][j].getItem()->getConfig()->getNAME();
+                string kode_tanaman = this->lahan.grid[i][j].getItem()->getConfig()->getKODE_HURUF();
+                if(temp_tanaman_unik.find(kode_tanaman) == temp_tanaman_unik.end()){
+                    temp_tanaman_unik[kode_tanaman] = nama_tanaman;
+                }
+            }
+        }
+    }
+
+    //Print tanaman yang unik
+    for(auto it = temp_tanaman_unik.begin(); it != temp_tanaman_unik.end();it++){
+            cout << it->first << " -> " << it->second << endl;
+        }
+
+
+    
+    //Detail Seperti 
+    // ALT -> Aloe Tree
+    // BNT -> Banana Tree
+    // CNT -> Coconut Tree
 }
 void Petani::TANAM(){  /* BELUM IMPLEMENTASI */
     cout << YELLOW << "\nCommand TANAM belum diimplementasikan!\n" << RESETstring << endl;
+    
+    //Pilih Tanaman Dari Inventory
+        //Cek dulu ada tanaman atau engga di inventory
+            //Throw Exception
+    cout << "Pilih tanaman dari penyimpanan " << endl;
+
+    //print inventory
+
+    
+    //Memilih Lokasi Dari Lahan
+        //Cek dulu lahannya sudah penuh atau belum
+            //Throw exception
+
+    //Hilangin tanaman dari inventory
+
 }
 void Petani::PANEN(){  /* BELUM IMPLEMENTASI */
     cout << YELLOW << "\nCommand PANEN belum diimplementasikan!\n" << RESETstring << endl;
+
+    // Print Ladang
+    // Validasi apakah ada tanaman yang sudah dapat di panen
+        //Throw Exception
+    //Tampilkan Jenis Tanaman Yang siap dipanen
+        // Pilih tanaman siap panen yang kamu miliki 
+        // 1. BNT (2 petak siap panen) 
+        // 2. ALT (3 petak siap panen)
+        // Nomor tanaman yang ingin dipanen: 2 // Lakukan validasi
+            //Validasi string or something
+            //valiasi apakah tanaman itu benar benar siap panen
+        // Berapa petak yang ingin dipanen: 3 // Lakukan validasi
+            //Validasi string or something
+            //Validasi petak tersebut siap panen atau tidak
+
+        //Jika lebih dari satu maka ada pilihan petak mana yang ingin dipanen
+            //validasi lagi petak valid atau tidak
+
+        //Validasi Inventory Cukup Atau Tidak
+
+
 }
